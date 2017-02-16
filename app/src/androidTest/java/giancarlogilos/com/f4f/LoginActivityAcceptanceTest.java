9aae4692dbfa1ad5fc2341f155db9573aebc6878
pofay <gianCarlo.gilos@outlook.com>
2017-02-16 10:07:56 +0800

    Failing Acceptance Test: User should be able to see Vendor Finder on login

--- app/src/androidTest/java/giancarlogilos/com/f4f/LoginActivityAcceptanceTest.java
+++ app/src/androidTest/java/giancarlogilos/com/f4f/LoginActivityAcceptanceTest.java
@@ -9,6 +9,13 @@ import android.widget.EditText;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertThat;
 
@@ -29,6 +36,21 @@ public class LoginActivityAcceptanceTest {
         assertThat(usernameField(), instanceOf(EditText.class));
         assertThat(passwordField(), instanceOf(EditText.class));
         assertThat(loginButton(), instanceOf(Button.class));
+    }
+
+    @Test
+    public void ItShouldMoveToTheVendorFinderWhenCredentialsAreAllowed() {
+        loginRule.launchActivity(new Intent());
+
+        String validUsername = "VALID_USERNAME";
+        String validPassword = "VALID_PASSWORD";
+
+        onView(withId(R.id.username_field)).perform(typeText(validUsername));
+        onView(withId(R.id.password_field)).perform(typeText(validPassword));
+
+        onView(withId(R.id.login_button)).perform(click());
+
+        onView(withId(R.id.vendor_finder_text)).check(matches(withText("VENDOR FINDER")));
     }
 
     private EditText usernameField() {
