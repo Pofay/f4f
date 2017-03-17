ac66ffc0b5db106e31e4701d144a15794dee4b7a
pofay <gianCarlo.gilos@outlook.com>
2017-03-17 18:34:40 +0800

    Added resource naming of widgets

--- app/src/main/java/giancarlogilos/com/f4f/SupplierProfileActivity.java
+++ app/src/main/java/giancarlogilos/com/f4f/SupplierProfileActivity.java
@@ -6,8 +6,22 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SupplierProfileActivity extends AppCompatActivity {
+
+    @BindView(R.id.user_profile_photo)
+    ImageButton userProfilePhotoButton;
+
+    @BindView(R.id.user_available_products)
+    TextView avaibleProductsText;
+
+    @BindView(R.id.user_comments_and_reviews)
+    TextView commentsAndReviews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +29,11 @@ public class SupplierProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_supplier_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ButterKnife.bind(this);
+
+
+
 
 
     }
